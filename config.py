2441055e56b92f8aacf4cae3d7074c8cbfdffba3
Karina-droid <meriina.karin@gmail.com>
2021-03-12 12:35:45 +0300

    preparatory steps for deployment

--- config.py
+++ config.py
@@ -8,14 +8,15 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     ADMINS = ['meriina.karin@gmail.com']
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     LANGUAGES = ['en', 'ru', 'he']
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = 'meriina.karin@gmail.com'
     MAIL_PASSWORD = '1Raketa!'
     POSTS_PER_PAGE = 25
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
