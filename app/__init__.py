bc80270fd05227d85cbe8ecb04969aa5a5aa1956
Karina-droid <meriina.karin@gmail.com>
2021-03-13 17:15:12 +0300

    deleted logs and mail part in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -43,7 +43,7 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
-    if not app.debug and not app.testing:
+     ''' if not app.debug and not app.testing:
         if app.config['MAIL_SERVER']:
             auth = None
             if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -71,7 +71,7 @@ def create_app(config_class=Config):
         app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
+        app.logger.info('Microblog startup') '''
 
     return app
 
